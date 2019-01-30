a1a48b2fd7794c31ec45f56b77bb8abb5bcc66d7
brhowey <42812674+brhowey@users.noreply.github.com>
2019-01-29 20:06:15 -0500

    Update update.php

--- update.php
+++ update.php
@@ -1,6 +1,6 @@
 <?php 
 	
-	require '../database/database.php';
+	require 'database.php';
 
 	$id = null;
 	if ( !empty($_GET['id'])) {
@@ -71,6 +71,9 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin="anonymous"></script>
     <link   href="css/bootstrap.min.css" rel="stylesheet">
     <script src="js/bootstrap.min.js"></script>
 </head>
